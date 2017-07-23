136f523483f8f621e91a80882c08839bc2cef23e
Gabriel Brolo <gabrolo13@gmail.com>
2017-07-22 22:31:04 -0600

    txt Generation

--- src/Run/Runnable.java
+++ src/Run/Runnable.java
@@ -3,16 +3,32 @@ package Run;
 import Implementation.AFN;
 import Syntax.PostFix;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by Gabriel Brolo on 22/07/2017.
  */
 public class Runnable {
+    public static String regexp;
     public static void main(String []args) {
-        AFN afn = new AFN("(a*|b*)c");
-        System.out.println(afn.getSymbolList());
-        System.out.println(afn.getTransitionsList());
-        System.out.println(afn.getFinalStates());
-        System.out.println(afn.getStates());
-        System.out.println(afn.getInitialState());
+        regexp = "(b|b)*abb(a|b)*";
+        writeFile();
+    }
+
+    public static void writeFile() {
+        try{
+            AFN afn = new AFN(regexp);
+            PrintWriter writer = new PrintWriter("AFN.txt", "UTF-8");
+            writer.println("REGULAR EXPRESSION: "+regexp);
+            writer.println("SYMBOL LIST: "+afn.getSymbolList());
+            writer.println("TRANSITIONS LIST: "+afn.getTransitionsList());
+            writer.println("FINAL STATE: "+afn.getFinalStates());
+            writer.println("STATES: "+afn.getStates());
+            writer.println("INITIAL STATE: "+afn.getInitialState());
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
